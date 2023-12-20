f6e52d214ad2bd4125c86bb38e3e2082b71dd2d8
RVCTF <110041401+LarissaLiou@users.noreply.github.com>
2023-12-20 19:27:47 +0800

    main changes
    idk why but these changes appeared even tho i did nothin :/

--- index.php
+++ index.php
@@ -10,4 +10,45 @@ include('templates/home.tpl.php');
 
 include('templates/defaults/end.tpl.php');
 
+//retrieving filename from url
+if (isset($_GET['filename'])){
+    $filename = $_GET['filename']; 
+}
+else{
+    $filename = "";
+}
+
+//bringing user to different pages
+if ($userInfo == true){
+    switch($filename){
+        case 'about':
+            include '/templates/defaults/header.tpl.php';
+            include '/templates/about.tpl.php';
+        break;
+
+        case 'rubrix':
+            include '/templates/defaults/header.tpl.php';
+            include '/templates/judgingRubrix.tpl.php';
+        break;
+
+        case 'pastGames':
+            include '/templates/defaults/header.tpl.php';
+            include '/templates/pastGames.tpl.php';
+        break;
+
+        case 'gallery':
+            include '/templates/defaults/header.tpl.php';
+            include '/templates/gallery.tpl.php';
+        break;
+
+        case 'game':
+            include '/templates/defaults/header.tpl.php';
+            include '/template/game.tpl.php';
+        break;
+
+        default:
+            include '/templates/defaults/header.tpl.php';
+            include('templates/home.tpl.php');
+    }
+}
 ?>
