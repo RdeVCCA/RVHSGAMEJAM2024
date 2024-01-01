2ae1eaa9bc36a8e198e771f1334b552098c6ca74
RVCTF <110041401+LarissaLiou@users.noreply.github.com>
2024-01-01 23:40:00 +0800

    gallery page backend append
    - php backend to take four details from database and echo onto page
    - still lack css for the organisation and game and interactiveness
    - lack redirection when clicking onto game
    
    - need do game page with two section of backends

--- static/js/library/gallery.js
+++ static/js/library/gallery.js
@@ -1,25 +1,31 @@
-var count = document.getElementById("count").value;
+// var count = document.getElementById("count").value;
+// var gameDetails = document.getElementById("gameDetails").value;
 
-var div = document.getElementById("appendGames");
+// var div = document.getElementById("appendGames");
 
-for (let i = 0; i < count; i++) {
-    let gameLogo = document.creatElement("div");
-    gameLogo.class = "gameLogo";
-    div.append(gameLogo);
-    let name = document.createElement("div");
-    name.class = "name";
-    div.append(name);
-    let creator = document.creatElement("span");
-    creator.class = "creator";
-    div.append(creator);
-    let genre = document.createElement("span");
-    genre.class = "genre";
-    div.append(genre);
-  }
+// for (let i = 0; i < count; i++) {
+//   console.log(count);
+//   let gameLogo = document.creatElement("div");
+//   gameLogo.class = "gameLogo";
+//   gameLogo.value = gameDetails[i][0]
+//   div.append(gameLogo);
+//   let name = document.createElement("div");
+//   name.class = "name";
+//   gameLogo.value = gameDetails[i][1]
+//   div.append(name);
+//   let creator = document.creatElement("span");
+//   creator.class = "creator";
+//   gameLogo.value = gameDetails[i][2]
+//   div.append(creator);
+//   let genre = document.createElement("span");
+//   genre.class = "genre";
+//   gameLogo.value = gameDetails[i][3]
+//   div.append(genre);
+// }
 
-  <div class = "game">
-            <div class = "gameLogo"></div>
-            <span class = "name"></span>
-            <span class = "creator"></span>
-            <span class = "genre"></span>
-        </div>
+{/* <div class = "game">
+    <div class = "gameLogo"></div>
+    <span class = "name"></span>
+    <span class = "creator"></span>
+    <span class = "genre"></span>
+</div> */}
--- templates/gallery.tpl.php
+++ templates/gallery.tpl.php
@@ -1,26 +1,46 @@
         <link rel = "stylesheet" href = "static/css/gallery.css">
+        <script src = "static/js/library/gallery.js"></script>
     </head>
-
-<?php 
-//require a games table to store all the 2023 game information
-$sql = 'SELECT COUNT(*) FROM games';
-$count = mysqli_query($conn, $sql);
-// echo($count);
-while($rowData = mysqli_fetch_array($count)){
-    $gamecount = intval($rowData[0].'<br>');
-    echo $gamecount;
-}
-
-foreach(range(1, $gamecount) as $index){
-    $sql_info = 'SELECT Thumbnail, GameName, Author, Genre FROM games WHERE Id = ?';
-    $LNCG = prepared_query($conn, $sql_info, [$index], 's');
-}
-
-
-// $count = prepared_query($conn, $sql, []);
-?>
+    
     <body>    
         <h1> Browse Games </h1>
-        <span id = "count" value = "<?php $count?>"></span>
-        <div id = "appendGames"></div>
+        <div id = "appendGames">
+            <?php 
+            //require to create a games table to store all the 2023 game information
+
+            //finding out the number of games created in 2023 gamejam
+            $sql = 'SELECT COUNT(*) FROM games';
+            $coun = ($conn ->query($sql))->fetch_all();
+            foreach($coun as $cou){
+                foreach($cou as $co){
+                    $count = $co; //array to string conversion
+                }
+            }
+
+            //finding the details of each of the gamejam and turning into an array
+            foreach(range(0, $count) as $index){
+                //fetching of four details for each of the games
+                $sql_info = 'SELECT Thumbnail, GameName, Author, Genre FROM games WHERE Id = ?';
+                $Thumbnail = "";
+                $GameName = "";
+                $Author = "";
+                $Genre = "";
+                $stmt = prepared_query($conn, $sql_info, [$index], 's');
+                $stmt->bind_result($Thumbnail, $GameName, $Author, $Genre);
+                $stmt->fetch();
+                mysqli_stmt_close($stmt);
+
+                //echo out the four details
+                echo "<img class = 'gameLogo' src = $Thumbnail></div>";
+                echo "<span class = 'name'>$GameName</span>";
+                echo "<span class = 'creator'>$Author</span>";
+                echo "<span class = 'genre'>$Genre</span>";
+            }
+
+            //inserting the details into value of html tags and then retrieving and inserting using js
+
+            // $count = prepared_query($conn, $sql, []);
+            ?>
+        </div>
     </body>
+<html>
