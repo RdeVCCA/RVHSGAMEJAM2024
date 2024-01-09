50ece2f7813b3e3e54b8c1c4b42a810d80e7da87
LarissaLiou <110041401+LarissaLiou@users.noreply.github.com>
2024-01-09 17:15:17 +0800

    9/1/2024 update 2
    - add most game page html structure
    - add css alignment and on hover
    - add js for rating system highlighting and clicking
    
    - still lack comments backend section
    - calculation of ratings and storing in database
    - added thumbnails and trailer in one line where it can be controlled with the arrows
    - download gamefiles after pressing play game

---
+++ static/js/game.js
@@ -0,0 +1,130 @@
+//change color of star when hover
+function highlightStar(star_number){
+    if (star_number > 0 && star_number < 6){
+        for (star_number; star_number > 0; star_number--){
+            document.getElementById("Star" + star_number).src = "static/img/ColoredStar.png"
+        }
+    } else if (star_number > 5 && star_number < 11){
+        for (star_number; star_number > 5; star_number--){
+            document.getElementById("Star" + star_number).src = "static/img/ColoredStar.png"
+        }
+    } else if (star_number > 10 && star_number < 16){
+        for (star_number; star_number > 10; star_number--){
+            document.getElementById("Star" + star_number).src = "static/img/ColoredStar.png"
+        }
+    } else{
+        for (star_number; star_number > 15; star_number--){
+            document.getElementById("Star" + star_number).src = "static/img/ColoredStar.png"
+        }
+    } 
+}
+
+//change color of star when not hover
+var highlighted1 = false
+var highlighted2 = false
+var highlighted3 = false
+var highlighted4 = false
+var permanentNumber1 = 0
+var permanentNumber2 = 0
+var permanentNumber3 = 0
+var permanentNumber4 = 0
+
+function unhighlightStar(star_number){
+    if (star_number > 0 && star_number < 6){
+        if (highlighted1 == false){
+            for (star_number; star_number > 0; star_number--){
+                document.getElementById("Star" + star_number).src = "static/img/Star.png"
+            }
+        } else{
+            for (star_number; star_number > permanentNumber1; star_number--){
+                document.getElementById("Star" + star_number).src = "static/img/Star.png"
+            }
+        }
+    } else if (star_number > 5 && star_number < 11){
+        if (highlighted2 == false){
+            for (star_number; star_number > 5; star_number--){
+                document.getElementById("Star" + star_number).src = "static/img/Star.png"
+            }
+        } else{
+            for (star_number; star_number > permanentNumber2; star_number--){
+                document.getElementById("Star" + star_number).src = "static/img/Star.png"
+            }
+        }
+    } else if (star_number > 10 && star_number < 16){
+        if (highlighted3 == false){
+            for (star_number; star_number > 10; star_number--){
+                document.getElementById("Star" + star_number).src = "static/img/Star.png"
+            }
+        } else{
+            for (star_number; star_number > permanentNumber3; star_number--){
+                document.getElementById("Star" + star_number).src = "static/img/Star.png"
+            }
+        }
+    } else{
+        if (highlighted4 == false){
+            for (star_number; star_number > 15; star_number--){
+                document.getElementById("Star" + star_number).src = "static/img/Star.png"
+            }
+        } else{
+            for (star_number; star_number > permanentNumber4; star_number--){
+                document.getElementById("Star" + star_number).src = "static/img/Star.png"
+            }
+        }
+    }
+}
+
+//change color of star when clicked on
+function permanentHighlight(star_number){
+    if (star_number > 0 && star_number < 6){
+        permanentNumber1 = star_number
+        for (let star_number = 5; star_number > 0; star_number--){
+            document.getElementById("Star" + star_number).src = "static/img/Star.png"
+        }
+        for (star_number; star_number > 0; star_number--){
+            document.getElementById("Star" + star_number).src = "static/img/ColoredStar.png"
+        }
+        highlighted1 = true
+    } else if (star_number > 5 && star_number < 11){
+        permanentNumber2 = star_number
+        for (let star_number = 10; star_number > 5; star_number--){
+        document.getElementById("Star" + star_number).src = "static/img/Star.png"
+        }
+        for (star_number; star_number > 5; star_number--){
+            document.getElementById("Star" + star_number).src = "static/img/ColoredStar.png"
+        }
+        highlighted2 = true
+    } else if (star_number > 10 && star_number < 16){
+        permanentNumber3 = star_number
+        for (let star_number = 15; star_number > 10; star_number--){
+        document.getElementById("Star" + star_number).src = "static/img/Star.png"
+        }
+        for (star_number; star_number > 10; star_number--){
+            document.getElementById("Star" + star_number).src = "static/img/ColoredStar.png"
+        }
+        highlighted3 = true
+    } else{
+        permanentNumber4 = star_number
+        for (let star_number = 20; star_number > 15; star_number--){
+            document.getElementById("Star" + star_number).src = "static/img/Star.png"
+            }
+        for (star_number; star_number > 15; star_number--){
+            document.getElementById("Star" + star_number).src = "static/img/ColoredStar.png"
+        }
+        highlighted4 = true
+    } 
+    
+    
+    
+}
+
+//defining variables
+var total_ratings = 0
+var number_of_ratings = 0
+var average_ratings = 0
+var current_ratings = 0
+//sets current rating when clicked on star
+function calculateRatings(ratings){
+    current_ratings = ratings
+    number_of_ratings += 1
+    average_ratings = (current_ratings / number_of_ratings).toFixed(1)
+}
--- templates/game.tpl.php
+++ templates/game.tpl.php
@@ -1,4 +1,6 @@
         <link rel = "stylesheet" href = "static/css/game.css">
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@700&Josefin+Sans&display=swap" rel="stylesheet">
+        <script src = "static/js/game.js"></script>
     </head>
 
     <body>
@@ -39,6 +41,38 @@
         <?php
         echo "<div id = 'Description'>$Descriptions</div>";
         ?>
-
-        <h2>Rate</h2>
-
+        <div class = "margin">
+            <h2>Rate</h2>
+            <img src = "static/img/star.png" class = "Stars" id = "Star1" onmouseover="highlightStar(1)" onmouseout="unhighlightStar(1)" onclick="calculateRatings(1), permanentHighlight(1)">
+            <img src = "static/img/star.png" class = "Stars" id = "Star2" onmouseover="highlightStar(2)" onmouseout="unhighlightStar(2)" onclick="calculateRatings(2), permanentHighlight(2)">
+            <img src = "static/img/star.png" class = "Stars" id = "Star3" onmouseover="highlightStar(3)" onmouseout="unhighlightStar(3)" onclick="calculateRatings(3), permanentHighlight(3)">
+            <img src = "static/img/star.png" class = "Stars" id = "Star4" onmouseover="highlightStar(4)" onmouseout="unhighlightStar(4)" onclick="calculateRatings(4), permanentHighlight(4)">
+            <img src = "static/img/star.png" class = "Stars" id = "Star5" onmouseover="highlightStar(5)" onmouseout="unhighlightStar(5)" onclick="calculateRatings(5), permanentHighlight(5)">
+            <div class = "ratings">
+                <div id = "Criteria1">Relatedness to Theme</div>
+                <div id = "Theme">
+                    <img src = "static/img/star.png" class = "Star" id = "Star6" onmouseover="highlightStar(6)" onmouseout="unhighlightStar(6)" onclick="calculateRatings(1), permanentHighlight(6)">
+                    <img src = "static/img/star.png" class = "Star" id = "Star7" onmouseover="highlightStar(7)" onmouseout="unhighlightStar(7)" onclick="calculateRatings(2), permanentHighlight(7)">
+                    <img src = "static/img/star.png" class = "Star" id = "Star8" onmouseover="highlightStar(8)" onmouseout="unhighlightStar(8)" onclick="calculateRatings(3), permanentHighlight(8)">
+                    <img src = "static/img/star.png" class = "Star" id = "Star9" onmouseover="highlightStar(9)" onmouseout="unhighlightStar(9)" onclick="calculateRatings(4), permanentHighlight(9)">
+                    <img src = "static/img/star.png" class = "Star" id = "Star10" onmouseover="highlightStar(10)" onmouseout="unhighlightStar(10)" onclick="calculateRatings(5), permanentHighlight(10)">
+                </div>
+                <div id = "Criteria2">Aesthetic</div>
+                <div id = "Aesthetic">
+                    <img src = "static/img/star.png" class = "Star" id = "Star11" onmouseover="highlightStar(11)" onmouseout="unhighlightStar(11)" onclick="calculateRatings(1), permanentHighlight(11)">
+                    <img src = "static/img/star.png" class = "Star" id = "Star12" onmouseover="highlightStar(12)" onmouseout="unhighlightStar(12)" onclick="calculateRatings(2), permanentHighlight(12)">
+                    <img src = "static/img/star.png" class = "Star" id = "Star13" onmouseover="highlightStar(13)" onmouseout="unhighlightStar(13)" onclick="calculateRatings(3), permanentHighlight(13)">
+                    <img src = "static/img/star.png" class = "Star" id = "Star14" onmouseover="highlightStar(14)" onmouseout="unhighlightStar(14)" onclick="calculateRatings(4), permanentHighlight(14)">
+                    <img src = "static/img/star.png" class = "Star" id = "Star15" onmouseover="highlightStar(15)" onmouseout="unhighlightStar(15)" onclick="calculateRatings(5), permanentHighlight(15)">
+                </div>
+                <div id = "Criteria3">Fun</div>
+                <div id = "Fun">
+                    <img src = "static/img/star.png" class = "Star" id = "Star16" onmouseover="highlightStar(16)" onmouseout="unhighlightStar(16)" onclick="calculateRatings(1), permanentHighlight(16)">
+                    <img src = "static/img/star.png" class = "Star" id = "Star17" onmouseover="highlightStar(17)" onmouseout="unhighlightStar(17)" onclick="calculateRatings(2), permanentHighlight(17)">
+                    <img src = "static/img/star.png" class = "Star" id = "Star18" onmouseover="highlightStar(18)" onmouseout="unhighlightStar(18)" onclick="calculateRatings(3), permanentHighlight(18)">
+                    <img src = "static/img/star.png" class = "Star" id = "Star19" onmouseover="highlightStar(19)" onmouseout="unhighlightStar(19)" onclick="calculateRatings(4), permanentHighlight(19)">
+                    <img src = "static/img/star.png" class = "Star" id = "Star20" onmouseover="highlightStar(20)" onmouseout="unhighlightStar(20)" onclick="calculateRatings(5), permanentHighlight(20)">
+                </div>
+            </div>
+            <h2>Comments</h2>
+        </div>
